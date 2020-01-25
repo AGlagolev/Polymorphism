565002fe69383a17da7410e611919ebb987a8c04
Alex <Alex@DESKTOP-CEQG6CC>
2020-01-25 18:10:18 +0200

    Add project files.

---
+++ Project3/Myfunc.cpp
@@ -0,0 +1,47 @@
+
+#include "Myfunc.h"
+
+
+
+
+//
+////// operator <<
+//std::ostream& operator<<(std::ostream& os, const Fraction& obj)
+//{
+//	os << obj.get_num() << " / " << obj.get_denum();
+//	return os;
+//}
+//////////////////////////////////////////////////////////////
+//std::ostream& operator<<(std::ostream& os, const String& obj)
+//{
+//	os << obj.;
+//	return os;
+//}
+//// operator <<
+//std::istream& operator>>(std::istream& is,Fraction& obj)
+//{
+//	int num, den;
+//	is >> num >> den;
+//	obj(num, den);
+//	return is;
+//}
+//
+//void Arr_clear(char* Arr)
+//{
+//	for (int i = 0; i < 300; i++)
+//	{
+//		*(Arr + i) = ' ';
+//	}
+//}
+////////////////////////////////////////////////
+//std::ostream& operator<<(std::ostream& os, const String& obj)
+//{
+//	for (int i = 0; i <= obj.get_len(); i++)
+//	{
+//		os << obj.get_Simb(i);
+//	}
+//	
+//	return os;
+//}
+
+///////////////////////////////////////////////
---
+++ Project3/Myfunc.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cmath>
+#include<iostream>
+#include<string>
+
+
+//double distance(const Point& A,const Point& B);
+//void parsw(Fraction& A);
+//bool comparison(const String& A, const String& B);
+//std::ostream& operator<<(std::ostream& os, const Fraction& obj);
+//std::istream& operator>>(std::istream& is, Fraction& obj);
+////std::ostream& operator<<(std::ostream& os, const String& obj);
+//void Arr_clear(char* Arr);
+//int fin_d(char Arr[], const String& A, const String& B);
